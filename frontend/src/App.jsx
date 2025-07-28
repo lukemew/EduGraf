976d0bf1eb6535786f9588ee25b3118f43e6b0ae
lukemew <lucasmelor65@gmail.com>
2025-07-28 12:38:32 -0300

    feat: adicionado novo componente Tips

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Banner from "./components/Banner/Banner";
 import Button from "./components/Button/Button";
+import Tips from "./components/Dicks/Tips";
 import Home from "./pages/Home/Home";
 
 import "./App.css";
@@ -11,7 +12,15 @@ function App() {
     <div>
       <Navbar />
       <Banner />
-      <Button filled={false} link={""} description={"Gerar tabelas do polo"} />
+      <div className="container">
+        <Button
+          filled={false}
+          link={""}
+          description={"Gerar tabelas do polo"}
+        />
+      </div>
+
+      <Tips pagina={1} />
     </div>
   );
 }
---
+++ frontend/src/components/Dicks/Tips.jsx
@@ -0,0 +1,42 @@
+import "./Tips.css";
+import Ilustracao2 from "../../assets/ilustracao2.png";
+
+const DicasHome = [
+  "Vá para a de opção criar tabelas",
+  "Irá ser criado a tabela de um polo",
+  "Agora vá para segunda opção",
+  "Vai ser gerado gráficos de vários polos",
+];
+const DicasTabela = [
+  "As planilhas devem estar no formato Excel (.xlsx).",
+  "Planilha preenchida com o modelo padrão",
+  "As planilhas deve conter,Nome da escola,Modalidade,Niveis de Leitura e Niveis de Escrita",
+];
+const DicasGrafico = [
+  "Primeiro é necessário fazer o processo de gerar tabelas",
+  "Após elas serem geradas, reúna todas as planilhas e as insira na outra funcionalidade para gerar o gráfico",
+];
+
+const Tips = ({ pagina }) => {
+  function seletorDeDicas() {
+    if (pagina == 1) {
+      return DicasHome.map((dica, index) => <li key={index}>{dica}</li>);
+    } else if (pagina == 2) {
+      return DicasTabela.map((dica, index) => <li key={index}>{dica}</li>);
+    } else if (pagina == 3) {
+      return DicasGrafico.map((dica, index) => <li key={index}>{dica}</li>);
+    }
+  }
+
+  return (
+    <div className="tips">
+      <h2>Dicas</h2>
+      <div className="tips-container">
+        <img src={Ilustracao2} alt="Ilustração da seção de dicas" />
+        <ul>{seletorDeDicas()}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default Tips;
