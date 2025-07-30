fdfc3818d08f8fe47c006bee9e10f97c1f214e40
lukemew <lucasmelor65@gmail.com>
2025-07-30 08:46:21 -0300

    feat: finalizado componente Select

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Banner from "./components/Banner/Banner";
-import Button from "./components/Button/Button";
-import Tips from "./components/Dicks/Tips";
-import Home from "./pages/Home/Home";
+import Select from "./components/Select/Select";
 
 import "./App.css";
-import SmallButton from "./components/SmallButton/SmallButton";
 
 function App() {
   return (
     <div>
-      <SmallButton filled={true} description={"Salvar Tabela"} />
+      <Select></Select>
     </div>
   );
 }
---
+++ frontend/src/components/Select/Select.jsx
@@ -0,0 +1,26 @@
+import "./select.css";
+
+const Select = () => {
+  return (
+    <div>
+      <select className="select" name="Seleção de polos">
+        <option value="1">Polo 1</option>
+        <option value="2">Polo 2</option>
+        <option value="3">Polo 3</option>
+        <option value="4">Polo 4</option>
+        <option value="5">Polo 5</option>
+        <option value="6">Polo 6</option>
+        <option value="7">Polo 7</option>
+        <option value="8">Polo 8</option>
+        <option value="9">Polo 9</option>
+        <option value="10">Polo 10</option>
+        <option value="11">Polo 11</option>
+        <option value="12">Polo 12</option>
+        <option value="13">Polo 13</option>
+        <option value="14">Polo 14 (Geral)</option>
+      </select>
+    </div>
+  );
+};
+
+export default Select;
